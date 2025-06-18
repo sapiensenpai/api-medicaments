6fdd877b81199bbbd495d86f1d4fd3e94cb54157
Isaac-Xavier Dale <isaacxdale@gmail.com>
2025-06-18 03:27:23 +0100

    Add search/drug/category routes with fuzzy search & pagination

---
+++ api/routes/category.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router  = express.Router();
+const data    = require('../../data/drugs.json');
+
+router.get('/:code', (req, res) => {
+  const results = data.filter(d => d.therapeutic_class === req.params.code);
+  res.json({ count: results.length, results });
+});
+
+module.exports = router;
