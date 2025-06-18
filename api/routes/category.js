5e969dfc432d7210cf8dfc658f62f3075f4d0921
Isaac-Xavier Dale <isaacxdale@gmail.com>
2025-06-18 03:54:50 +0100

    Add drugs.json and finalize routes for Render deployment

--- api/routes/category.js
+++ api/routes/category.js
@@ -4,7 +4,7 @@ const data    = require('../../data/drugs.json');
 
 router.get('/:code', (req, res) => {
   const results = data.filter(d => d.therapeutic_class === req.params.code);
-  res.json({ count: results.length, results });
+  res.json({ total: results.length, results });
 });
 
 module.exports = router;
